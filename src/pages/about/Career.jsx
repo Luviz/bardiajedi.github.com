cd81c2e558cac5db81b188a1365c4f38538102fd
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-02-06 17:24:32 +0100

    update career

--- src/pages/about/Career.jsx
+++ src/pages/about/Career.jsx
@@ -4,13 +4,50 @@ export default function Career() {
     return <div>
         <hr />
         <h3>
-            Affecto 2017~2019 Feb
+            Affecto 2017 – 2019 February
         </h3>
         <p>
-            I stared me Career at Affecto, as an interner quick signed my contract and started working there.
+            I started my career at Affecto Sweden AB, on the 10th of October 2016 as an intern.
+            My internship was for a 2-months period, but I did sign my contract mid-November.
         </p>
         <p>
-            in 2018 Affecto was pruches by CGI, not nothing changed more the the seanery ...
+            From there, it was a rapid climb up words.
+            Within 2 years I went from a junior position to lead roles.
+            If I remember it right, I was never a junior developer. I can say that now,
+            that I have a few jr. devs, under my wing.
+            I have to say skipping that step is a double-edged sword,
+            for one end I didn’t have access to the vast knowledge of an experience senior dev.
+            However, I did have access to a few brilliant software devs,
+            but because I didn’t get the opportunity to work with them closely.
+            I had to build up that knowledge base by myself, mostly.
+        </p>
+        <p>
+            So, a good example is that I have written a lot of code what works just fine.
+            But it wasn’t built to expand upon.
+        </p>
+        <p>
+            The fact that I was able to take that journey mostly alone have made me into a better developer.
+            For instance, I can say I have tried that, and it didn’t work, because of this and that.
+            Instead of saying we should not do that because that guy told me it’s a bad idea.
+            Having 1st hand experience affects you in a different way.
+        </p>
+        <p>
+            On my last interview with the woman that recruited me, I asked her why? <br />
+            <i> Her answer was:</i> <br />
+            <i>Efficiency! You didn't need handholding so;
+            we just gave you tuff challenges and saw your grin widen.</i> <br />
+            Not a direct quote, but if you call her,
+            I don’t think she would say anything different.
+            Also, she said it in Swedish, so I had to translate.
+
+        </p>
+        <p>
+            For more details read the projects below.
+        </p>
+        <p>
+            Also, CGI bought Enfo 2018 and we got merged in 2019,
+            more than that nothing actually changed. There were some policy changes,
+            some new rules. But other than that my work did not change.
         </p>
         <hr />
         <h3>
