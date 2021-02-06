b73d4e4c10595d0324f97f01016185071703edb7
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-02-06 17:45:29 +0100

    added links to gh ln

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -11,12 +11,15 @@ export function NavItems(props){
         <NavItem text="Home" href="#" icon=""/>
         <NavItem text="Blog" href="#Blog" icon=""/>
         <NavItem text="About" href="#About" icon=""/>
+        <li className="nav-spacer"></li>
+        <NavItem text="Likedin" href="https://www.linkedin.com/in/bardia-jedi-3b2699b8/" target={"bjblog-ln"} icon=""/>
+        <NavItem text="Github" href="https://github.com/luviz" target={"bjblog-gh"} icon=""/>
     </ul>
 }
 
 export function NavItem(props){
     return <li className="nav-listItem">
-        <a href={props.href}>
+        <a href={props.href} target={props.target}>
             {/* // icon */}
             <span>{props.text}</span>
         </a>
