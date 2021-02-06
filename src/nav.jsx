af26e1859caa036aa5175dc2bf523e9ddaf29a8b
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-02-06 18:13:14 +0100

    added nav icons and stlying

--- src/nav.jsx
+++ src/nav.jsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React from 'react';
+import {GithubSvg, LinkedinSvg} from './assets/svg/NavIcons'
 
 export default function Nav(prop) {
     return <nav>
@@ -12,8 +13,12 @@ export function NavItems(props){
         <NavItem text="Blog" href="#Blog" icon=""/>
         <NavItem text="About" href="#About" icon=""/>
         <li className="nav-spacer"></li>
-        <NavItem text="Likedin" href="https://www.linkedin.com/in/bardia-jedi-3b2699b8/" target={"bjblog-ln"} icon=""/>
-        <NavItem text="Github" href="https://github.com/luviz" target={"bjblog-gh"} icon=""/>
+        <NavItemIcon text="Likedin" href="https://www.linkedin.com/in/bardia-jedi-3b2699b8/" target={"bjblog-ln"} >
+            <LinkedinSvg/>
+        </NavItemIcon>
+        <NavItemIcon text="Likedin" href="https://www.linkedin.com/in/bardia-jedi-3b2699b8/" target={"bjblog-ln"} icon="">
+            <GithubSvg/>
+        </NavItemIcon>
     </ul>
 }
 
@@ -22,6 +27,15 @@ export function NavItem(props){
         <a href={props.href} target={props.target}>
             {/* // icon */}
             <span>{props.text}</span>
+        </a>
+    </li>
+}
+
+export function NavItemIcon(props){
+    return <li className="nav-listItemIcon">
+        <a href={props.href} target={props.target}>
+            {/* // icon */}
+            {props.children}
         </a>
     </li>
 }
