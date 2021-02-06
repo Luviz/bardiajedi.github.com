8ef657768a2065e4d1208875e29b1bb1531669d2
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-02-06 10:01:03 +0100

    fixing file names

---
+++ src/Nav.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+export default function Nav(prop) {
+    return <nav>
+       <NavItems/>
+    </nav>
+}
+
+export function NavItems(props){
+    return <ul>
+        <NavItem text="Home" href="#" icon=""/>
+        <NavItem text="Blog" href="#Blog" icon=""/>
+        <NavItem text="About" href="#About" icon=""/>
+    </ul>
+}
+
+export function NavItem(props){
+    return <li className="nav-listItem">
+        <a href={props.href}>
+            {/* // icon */}
+            <span>{props.text}</span>
+        </a>
+    </li>
+}
