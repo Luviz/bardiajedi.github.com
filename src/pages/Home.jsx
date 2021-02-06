91c29a1b5765945b72fe67416e05ea55c4d9ba76
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-02-06 02:44:43 +0100

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,25 @@ import * as React from 'react';
 
 export default function Home() {
     return <main>
-        <h1>Home</h1>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-        <ol>
-            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-            <li>Aliquam tincidunt mauris eu risus.</li>
-            <li>Vestibulum auctor dapibus neque.</li>
-        </ol>
+        <article>
+            <h3>
+                Hello, and welcome!
+            </h3>
+            <p>
+                I'm Bardia Jedi, Software dev amongst other things! 😉
+            </p>
+            <p>
+                This site will contain all my expreince and serve as a CV, 
+                at this moment the site is undergoing have work and much to be desried. 
+                But as time gose on, I hope that this will become way cooler then this. 
+            </p>
+        </article>
+        <article>
+            <h3>About this site</h3>
+            <p>
+                When this site is published it will be host on Github pages.
+            </p>
+            
+        </article>
     </main>
 }
