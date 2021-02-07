31a4a608c3445f8d914181fd5795e4b69d369793
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-02-07 13:01:30 +0100

    added publish

---
+++ webpack.prod.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: "production",
+  entry: {
+    index: './src/index.js',
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/react']
+          }
+        }
+      },
+      {
+        test:/.css$/,
+        use: ['style-loader','css-loader']
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Bardia Jedi"
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: true })
+  ],
+  output: {
+    filename: '[name].main.js',
+    path: path.resolve(__dirname, 'out'),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+};
