1ff2bf2f2a0aa2d411d2b9b9d18f830f8cce59c0
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-02-07 09:32:22 +0100

    cogs and animation

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,12 @@
 import * as React from 'react';
-export default function NotFound(){
-    return <span>404 - woops!</span>
+import { Cog, CogPathOnly } from '../assets/svg/NavIcons'
+export default function NotFound() {
+    return <main className="notFound">
+        <span>404 - woops!</span>
+        <div className="nf-art">
+            <Cog className="nf-art-cog1"/>
+            <Cog className="nf-art-cog2"/>
+            <Cog className="nf-art-cog3"/>
+        </div>
+    </main>
 }
