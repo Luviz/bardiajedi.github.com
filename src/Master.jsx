9807355d1cb61cbe1527b2a8d111fd334c565f8b
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-02-07 10:19:40 +0100

    some clean up

--- src/Master.jsx
+++ src/Master.jsx
@@ -1,11 +1,11 @@
 // master layout
-import React,{createContext, useContext, useEffect, useState} from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import Nav from './Nav';
 import * as Pages from './pages/index';
-import stlyes from './styles.css'
+import stlyes from './styles.css';
+import NotFound from './pages/NotFound';
 
 export default function Master(p) {
-    console.log("master")
     return <div>
         <Nav />
         <PageRouter />
@@ -15,12 +15,12 @@ export default function Master(p) {
 function PageRouter() {
     const [_, forceUpdate] = useState("true");
     var currPage = window.location.href.split("#")[1] || "Home";
-    useEffect(()=>window.addEventListener('popstate', (a) => { console.log(a), forceUpdate(new Date())}), [])
-        document.title = `Bardia Jedi ${currPage}`;
+    useEffect(() => window.addEventListener('popstate', (a) => { forceUpdate(new Date()) }), [])
+    document.title = `Bardia Jedi ${currPage}`;
     try {
         return Pages[currPage].default();
     } catch (e) {
         console.error(e);
-        return Pages.NotFound.default();
+        return <NotFound mess={e.message}/>
     }
 }
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,24 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Cog, CogPathOnly } from '../assets/svg/NavIcons'
-export default function NotFound() {
+import Article from '../Components/Artical';
+export default function NotFound(p) {
+    const [showError, setError] = useState(true);
     return <main className="notFound">
-        <span>404 - woops!</span>
         <div className="nf-art">
-            <Cog className="nf-art-cog1"/>
-            <Cog className="nf-art-cog2"/>
-            <Cog className="nf-art-cog3"/>
+            <Cog className="nf-art-cog1" />
+            <Cog className="nf-art-cog2" />
+            <Cog className="nf-art-cog3" />
         </div>
+        <h3 onClick={() => setError(!showError)}>
+            It seems like something's stuck!
+        </h3>
+        <div hidden={showError}>
+            {p?.mess}
+        </div>
+        <Article lable="Looking for my blog?">
+            <p>
+                My blog need to be migrated over to here. <a href="https://perspective.bardiajedi.com/">Blog</a>
+            </p>
+        </Article>
     </main>
 }
