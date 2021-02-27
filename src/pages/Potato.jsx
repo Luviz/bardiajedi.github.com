218b29e49b726f1a0628ad08ecf1cc03de052bfc
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-02-27 16:21:45 +0100

    The Grand potato holders Table

--- src/pages/Potato.jsx
+++ src/pages/Potato.jsx
@@ -1,21 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import img from '../assets/the-potato.png'
-
-function PotatoForm(p) {
-
-    return <from className={"potato-form-container"}>
-        <label htmlFor="userAlias">Alias:</label>
-        <input autoComplete="off" type="text" name="userAlias" id="userAlias" />
-        <input type="submit" value="Take the Potato" onClick={console.log}/>
-    </from>
-}
+import { CleanDateTime } from "../util";
 
 export default function Potato() {
+    return <PotatoContent />
+}
+
+function PotatoForm(p) {
+    const { href } = window.location;
+    const keyPair = href.split('#')[1].split('=');
+    if (keyPair.length < 2) {
+        return <div>
+            I don't think you're it! <br />
+            The Grand potato holders can be found at the bottom of the page
+        </div>
+    }
+    if (!p.latest) {
+        return <div>Gettting the last Potato holeder</div>
+    }
+    if (p.latest.RowKey === keyPair[1]) {
+
+        return <form className={"potato-form-container"} onSubmit={e => e.preventDefault()}>
+            <label htmlFor="userAlias">Alias:</label>
+            <input autoComplete="off" type="text" name="userAlias" id="userAlias" />
+            <input type="submit" value="Take the Potato" onClick={console.log} />
+        </form>
+    } else {
+        return <div>Sorry, But there is no potato here anymore!</div>
+    }
+}
+
+function getPotatoHolder() {
+    try {
+        return fetch("https://bj-potato.azurewebsites.net/api/potato").then(res => res.json());
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+function TableRow({ Name, CreateAt, RowKey }) {
+    return <tr>
+        <td>{Name}</td>
+        <td>{CleanDateTime(CreateAt)}</td>
+        <td>{RowKey}</td>
+    </tr>
+}
+
+function TheGrandPotatoHolders({ potatoHolders }) {
+    if (potatoHolders && [potatoHolders.length > 0]) {
+        const rows = potatoHolders.map(p => <TableRow {...p} />)
+        return <div className="table-container">
+            <table cellSpacing={0}>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Got Potato At</td>
+                        <td>Code</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+        </div>
+    } else {
+        return <div>working maybe?</div>
+    }
+}
+
+function PotatoContent() {
+    const [potatoholders, setPotatoHolder] = useState([])
+    useEffect((_) => {
+        getPotatoHolder().then(setPotatoHolder).catch(console.error);
+    }, []);
+    if (potatoholders.length > 1) {
+        potatoholders.sort((p1, p2) => new Date(p2.CreateAt) - new Date(p1.CreateAt));
+    }
+
     return <main>
         <article>
+
             <h1>You are it!</h1>
             <div className={"img-container"}>
-                <img src={img} alt="" srcset="" />
+                <img src={img} alt="Potato" />
             </div>
             <h3>Wait What...</h3>
             <p>
@@ -29,7 +96,7 @@ export default function Potato() {
                 <li>Think of a target.</li>
                 <li>Make them click the link!</li>
             </ol>
-            <PotatoForm />
+            <PotatoForm latest={potatoholders[0]} />
             <p>
                 Give me a bit a of break the game is quick limited at the moment,
                 and I would like you not use shotgun attacks. <br />
@@ -51,6 +118,10 @@ export default function Potato() {
                 You should probably do so!<br />
                 Note that you can get to this page via #Potato, just live the code out of the url.
             </p>
+        </article>
+        <article>
+            <h1>The Grand potato holders</h1>
+            <TheGrandPotatoHolders potatoHolders={potatoholders} />
         </article>
     </main>
 }
--- src/util.js
+++ src/util.js
@@ -1,3 +1,9 @@
 export function meh(){
     return "MEH.!!!"
 }
+
+export function CleanDateTime(ticks){
+    const date = new Date(ticks);
+
+    return `${date.getFullYear()}-${date.getMonth()+1}-${date.getDay()} ${date.getHours()}:${date.getMinutes()}`
+}
